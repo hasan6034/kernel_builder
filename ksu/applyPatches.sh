331c1902d19d947b555e8526d3d831186e5c9a3a
fukiame <fukiame@proton.me>
2024-05-07 17:57:24 +0700

    ksu: applyPatches: hardcode KSU version to 0.9.3
    ref: https://t.me/KernelSU/66
    Signed-off-by: fukiame <fukiame@proton.me>

--- ksu/applyPatches.sh
+++ ksu/applyPatches.sh
@@ -6,7 +6,7 @@ export maindir="$(pwd)"
 export outside="${maindir}/.."
 source "${outside}/env"
 
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s 0.9.3
 KSU_git_ver=$(cd KernelSU && git rev-list --count HEAD)
 KSU_ver=$(($KSU_git_ver + 10000 + 200))
 
